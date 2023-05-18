38dbf5f5f4f5298f5ef1689855be8e634a052441
kelvin901 <wawerukelvin2019@gmail.com>
2023-05-18 21:29:26 +0300

    Ungraded

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+    def talk
+      puts "Hello World!"
+    end
+  end
+ 
+  person = Person.new
+  
+
+  person.talk
+  def walk
+    puts "The Person is walking"
+  end
+
+  person.walk
